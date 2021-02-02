edf6d8648841e86275960d7b094ae1670a1ab2ec
jason song <jason@songyongdengdeMacBook-Pro.local>
2021-02-02 22:13:15 +0800

    ESLint 自动修复的问题
    ESLint 自动修复的问题

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 /**
- * app.js
- * 初始化相关服务和注入相关中间件
+ * FilePath: /jasonsoft-koa-server/app.js
+ * TODO: 初始化相关服务和注入相关中间件
  * Added by Jason.Song (成长的小猪) on 2021/01/31
- * CSDN: https://blog.csdn.net/jasonsong2008
- * GitHub: https://github.com/JasonSoft-Net
+ * ? CSDN: https://blog.csdn.net/jasonsong2008
+ * ? GitHub: https://github.com/JasonSoft-Net
  */
 
- /** 引入Koa框架 */
+/** 引入Koa框架 */
 import Koa from 'koa';
 
 /** 初始化Koa */
 const app = new Koa();
 
-app.use(async ctx => {
+app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
 /** 监听指定端口，启动服务 */
-app.listen(3000, ()=> {
-  console.log(`[\x1B[36mRunning\x1B[0m] 服务已启动：http://localhost:3000`);
+app.listen(3000, () => {
+  console.log('[\x1B[36mRunning\x1B[0m] 服务已启动：http://localhost:3000');
 });
--- bin/start.js
+++ bin/start.js
@@ -1,14 +1,14 @@
 /**
- * bin/start.js
- * 项目入口文件，初始相关配置信息
+ * FilePath: /jasonsoft-koa-server/bin/start.js
+ * TODO: 项目入口文件，初始相关配置信息
  * Added by Jason.Song (成长的小猪) on 2021/02/01
- * CSDN: https://blog.csdn.net/jasonsong2008
- * GitHub: https://github.com/JasonSoft-Net
+ * ? CSDN: https://blog.csdn.net/jasonsong2008
+ * ? GitHub: https://github.com/JasonSoft-Net
  */
 
 /**
- * 支持ES6/ES2015及更高版本的新特性和语法
- * 注意：这个必须放在当前入口文件的最上面
+ * * 支持ES6/ES2015及更高版本的新特性和语法
+ * ! 注意：这个必须放在当前入口文件的最上面
  * Added by Jason.Song (成长的小猪) on 2021/02/01 20:16:17
  */
 require('@babel/register')({
@@ -17,7 +17,7 @@ require('@babel/register')({
 });
 
 /**
- * 导入 app 初始化相关服务和注入相关中间件
+ * * 导入 app 初始化相关服务和注入相关中间件
  * Added by Jason.Song (成长的小猪) on 2021/02/01 20:21:01
  */
-const app = require('../app');
+module.exports = require('../app');
